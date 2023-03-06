60b4238039c95f567c0453bac6ad5147e616fffb
Elmeddin Mirzeyev <118839615+Elmeddin996@users.noreply.github.com>
2023-03-06 18:59:34 +0400

    stringMethods

---
+++ JS-string-methods/main.js
@@ -0,0 +1,91 @@
+//STRING METHODS
+
+//stringLength
+
+// const text="qwertyuiopasdfghjklzxcvbnm";
+// const textLength=text.length;
+
+// console.log(textLength);
+
+
+
+
+//stringSlice
+
+// const text="Hello how are you?";
+// const textSlice=text.slice(6,13);
+
+// console.log(textSlice);
+
+
+
+
+//stringReplace
+
+// let text="Hello Code Academy";
+// console.log(text);
+// text=text.replace("Code Academy", "P328");
+// console.log(text);
+
+
+
+//stringConcat
+
+// const text1 = "Hello";
+// const text2 = "P328";
+// const text3 = text1.concat(" ",text2);
+
+// console.log(text3);
+
+
+
+// stringTrim
+
+// const text = "       Hello  P328        ";
+// const text2 = text.trim();
+// console.log(text2);
+
+
+
+//stringSplit
+
+// const text = "Hello P328";
+// const textArray=text.split("");
+// const textArray2=text.split(" ");
+
+// //['H','e','l','l','o','','P','3','2','8']
+// console.log(textArray); 
+// //['Hello', 'P328']
+// console.log(textArray2);
+
+
+
+
+//stringCharAt
+
+// const text = "Hello P328";
+// const char = text.charAt(3);
+
+// console.log(char);
+
+
+
+
+//stringCodeAt
+
+// const text = "Hello World";
+// const unicode =text.charCodeAt(7); 
+
+// console.log(unicode);
+
+
+//stringIndexOf
+
+// const text = "Hello P328";
+// console.log(text.indexOf("P328"));
+
+
+//stringSubstring
+
+const text = "Hello World";
+console.log(text.substring(2,9));
